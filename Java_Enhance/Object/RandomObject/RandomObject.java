7bb5f923250cb494e0b54a25fbf4bf14bbce8629
keviness <kevin1737@163.com>
2020-10-19 23:38:35 +0800

    Add: Random Object

---
+++ Java_Enhance/Object/RandomObject/RandomObject.java
@@ -0,0 +1,49 @@
+/**Random Object example
+*@author keviness
+*@version 2020/10/19
+*/
+
+import java.util.*;
+
+public class RandomObject
+{
+    public static void main(String[] args)
+    {
+        Random rd = new Random();
+        int[] array = new int[5];
+        int value = -1;
+        int index = 0;
+        for (int j=0; j<array.length; j++)
+        {
+            array[j] = -1;
+        }
+
+        while (index<array.length)
+        {
+            value = rd.nextInt(6);
+            if (!IsInArray(array, value))
+            {
+                array[index] = value;
+                index++;
+            }
+        }
+
+        //show the array content
+        for (int element : array)
+        {
+            System.out.println(element);
+        }
+    }
+
+    public static boolean IsInArray(int[] array, int value)
+    {
+        for (int element : array)
+        {
+            if (element == value)
+            {
+                return true;
+            }
+        }
+        return false;
+    }
+}
