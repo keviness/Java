0caa765c256ea95a444670283f72943e4984b177
keviness <kevin1737@163.com>
2020-10-22 23:08:52 +0800

    Update: enumerate object example

--- Java_Enhance/Object/EnumerateObject.java
+++ Java_Enhance/Object/EnumerateObject.java
@@ -1,18 +1,36 @@
 /**Enumerate Object example
-*@author kevin
+*@author keviness
 *@version 2020/10/21
 */
+
 import java.util.*;
 
 public class EnumerateObject {
     public static void main(String[] args) {
-        Weekday day = Weekday.SUN;
+        Weekday day = Weekday2.SUN;
         System.out.println("The index: "+day.ordinal());  //return the index of constant value
         System.out.println("The name: "+day.name());  //return the enum obj's name
+        
         if (day.dayValue == 6 || day.dayValue == 0) {
             System.out.println("Work at home!");
         } else {
             System.out.println("Work at office!");
+        }
+        //switch statements
+        switch(day) {
+        case MON:
+        case TUE:
+        case WED:
+        case THU:
+        case FRI:
+            System.out.println("Today is " + day + ". Work at office!");
+            break;
+        case SAT:
+        case SUN:
+            System.out.println("Today is " + day + ". Work at home!");
+            break;
+        default:
+            throw new RuntimeException("cannot process " + day);
         }
     }
 }
@@ -24,5 +42,23 @@ enum Weekday {
 
     private Weekday(int dayValue) {
         this.dayValue = dayValue;
+    }
+}
+
+
+enum Weekday2 {
+    MON(1, "Monday"), TUE(2, "Tuesday"), WED(3, "Wednesday"), THU(4, "Thursday"), FRI(5, "Friday"), SAT(6, "Saturday"), SUN(0, "Sunday");
+
+    public final int dayValue;
+    private final String chinese;
+
+    private Weekday2(int dayValue, String chinese) {
+        this.dayValue = dayValue;
+        this.chinese = chinese;
+    }
+
+    @Override
+    public String toString() {
+        return this.chinese;
     }
 }
