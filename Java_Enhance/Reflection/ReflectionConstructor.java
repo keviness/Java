31db8097ae8f8461a7ca522c645b9f1edbe792b3
keviness <kevin1737@163.com>
2020-10-30 23:19:56 +0800

    Update: reflection example

---
+++ Java_Enhance/Reflection/ReflectionConstructor.java
@@ -0,0 +1,34 @@
+/**Reflection constructor example
+*@author keviness
+*@version 2020/10/30
+*/
+
+import java.lang.reflect.Constructor;
+
+public class ReflectionConstructor {
+    public static void main(String[] args) throws Exception {
+        // 获取构造方法Integer(int):
+        Constructor cons1 = Integer.class.getConstructor(int.class);
+        // 调用构造方法:
+        Integer n1 = (Integer) cons1.newInstance(123);
+        System.out.println(n1);
+
+        // 获取构造方法Integer(String)
+        Constructor cons2 = Integer.class.getConstructor(String.class);
+        Integer n2 = (Integer) cons2.newInstance("456");
+        System.out.println(n2);
+    }
+}
+
+
+//get superclass
+public class Main {
+    public static void main(String[] args) throws Exception {
+        Class i = Integer.class;
+        Class n = i.getSuperclass();
+        System.out.println(n);
+        Class o = n.getSuperclass();
+        System.out.println(o);
+        System.out.println(o.getSuperclass());
+    }
+}
