a758c98145d70e8d3bb6b94e1da2acfdcd4d8c7c
keviness <kevin1737@163.com>
2020-11-03 22:57:20 +0800

    Update: Generic examples

---
+++ Java_Enhance/Generic/GenericWrite.java
@@ -0,0 +1,30 @@
+/**Generic write
+*@author keviness
+*@version 2020/11/3
+*/
+
+public class GenericWrite {
+    public static void main(String[] args) {
+        Pair<String> p1 = new Pair<>("Hello", "world");
+        Pair<Integer> p2 = new Pair<>(123, 456);
+        Class c1 = p1.getClass();
+        Class c2 = p2.getClass();
+        System.out.println(c1==c2); // true
+        System.out.println(c1==Pair.class); // true
+    }
+}
+
+class Pair<T> {
+    private T first;
+    private T last;
+    public Pair(T first, T last) {
+        this.first = first;
+        this.last = last;
+    }
+    public T getFirst() {
+        return first;
+    }
+    public T getLast() {
+        return last;
+    }
+}
