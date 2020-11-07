cf0086916fc3324c08341603a0be51456407b44c
keviness <kevin1737@163.com>
2020-11-07 22:34:25 +0800

    Update: collection-properties examples

---
+++ Java_Enhance/Collection/CollectionProperties.java
@@ -0,0 +1,21 @@
+/**Collection: Properties example
+*@author keviness
+*@version 2020/11/7
+*/
+
+import java.io.*;
+import java.util.Properties;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        String settings = "# test" + "\n" + "course=Java" + "\n" + "last_open_date=2019-08-07T12:35:01";
+        ByteArrayInputStream input = new ByteArrayInputStream(settings.getBytes("UTF-8"));
+        Properties props = new Properties();
+        props.load(input);
+
+        System.out.println("course: " + props.getProperty("course"));
+        System.out.println("last_open_date: " + props.getProperty("last_open_date"));
+        System.out.println("last_open_file: " + props.getProperty("last_open_file"));
+        System.out.println("auto_save: " + props.getProperty("auto_save", "60"));
+    }
+}
