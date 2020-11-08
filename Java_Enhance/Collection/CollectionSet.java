4c70fba2bc5813ed0f8ce3b5b4aaf3929ed74fd7
keviness <kevin1737@163.com>
2020-11-08 23:43:37 +0800

    Update: Collection set

---
+++ Java_Enhance/Collection/CollectionSet.java
@@ -0,0 +1,30 @@
+/**Collection set example
+*@author keviness
+*@version 2020/11/8
+*/
+
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) {
+        //Hash Set
+        Set<String> set = new HashSet<>();
+        set.add("apple");
+        set.add("banana");
+        set.add("pear");
+        set.add("orange");
+        for (String s : set) {
+            System.out.println(s);
+        }
+
+        //Tree Set
+        Set<String> set = new TreeSet<>();
+        set.add("apple");
+        set.add("banana");
+        set.add("pear");
+        set.add("orange");
+        for (String s : set) {
+            System.out.println(s);
+        }
+    }
+}
